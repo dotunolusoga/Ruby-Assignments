76be18b3489d8120c609f5ba58ab9cb8ca93ecfa
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-04-22 18:24:49 -0500

    fixed name capitalization error

--- cheers.rb
+++ cheers.rb
@@ -1,7 +1,8 @@
 
+
 def ask_for_name
   puts "Hello, whats your name?"
-  gets.chomp.upcase
+  gets.chomp
 end
 
 name = ""
@@ -13,9 +14,8 @@ end
 name.gsub!(/[^\w]/, "")
 
 an_letters= "AEFHILMNORSX"
-name.each_char do |letter|
+name_case = name.upcase.each_char do |letter|
   article = an_letters.include?(letter) ? "an" : "a"
   puts "Give me #{article}...#{letter}!"
 end
 puts name + "'s just AWESOME!"
-

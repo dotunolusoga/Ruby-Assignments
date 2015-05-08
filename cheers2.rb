eb7c06e687d8a59378b66a12b997fe0f9b0c8537
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-05-07 22:23:20 -0500

    cheers2 corrected

--- cheers2.rb
+++ cheers2.rb
@@ -2,19 +2,17 @@
 require "date"
 
 def ask_for_birthday
-  puts "Hey, whats your birthday?"
-  gets.chomp.upcase
+  puts "Hey, whats your birthday?[MM/DD]"
+
+  birthday = gets.chomp
+
+  while birthday.empty?
+    puts "Hey, whats your birthday?[MM/DD]"
+  else
+    birth_day = Date.parse(birthday)
+    todays_date = Date.today
+    $b_days = (birth_day - todays_date) % 365
+    bday_count = $b_days.to_i
+  end
+  puts "Awesome!  Your birthday is in #{bday_count} days! Happy Birthday in advance!"
 end
-
-birthday = ""
-
-while birthday.empty?
- birthday = ask_for_birthday
-end
-
-birth_day = Date.parse(birthday)
-todays_date = Date.today
-$b_days= (birth_day - todays_date) % 365
-bday_count = $b_days.to_i
-
-puts "Awesome!  Your birthday is in #{bday_count} days! Happy Birthday in advance!"
